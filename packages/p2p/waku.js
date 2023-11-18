7545b06d1e89777bbc5c15e3556aa9fd7f33503e
Nicolas "Norswap" Laurent <norswap@gmail.com>
2023-11-18 17:56:01 +0300

    demo for protobuf-based messaging with waku relay node

--- packages/p2p/waku.js
+++ packages/p2p/waku.js
@@ -6,31 +6,37 @@ import {
   waitForRemotePeer,
   createDecoder,
   createEncoder,
-  bytesToUtf8,
-  utf8ToBytes,
   createRelayNode,
 } from "@waku/sdk"
 
 import protobuf from "protobufjs"
 
+// =================================================================================================
+
 // Every Waku Message has a content topic that categorizes it.
 // It is always encoded in clear text.
 // Recommended topic format: `/dapp-name/version/functionality/codec`
+
+// =================================================================================================
+
 // Codecs: `proto` for protobuf, `utf-8` for simple utf-8 strings.
+// const chatTopic = "/vapor/1/chat/proto"
+
+// // Create a message structure using Protobuf
+// const ChatMessage = new protobuf.Type("ChatMessage")
+//   .add(new protobuf.Field("timestamp", 1, "uint64"))
+//   .add(new protobuf.Field("sender", 2, "string"))
+//   .add(new protobuf.Field("message", 3, "string"))
+
+// =================================================================================================
+
 const systemTopic = "/vapor/1/system/proto"
-const chatTopic = "/vapor/1/chat/proto"
 
 // NOTE: Do we need `ephemeral = true`? (default is `false`).
 // Probably not because we're not running a storage node.
 // If we want to support offline messaging, then we can use a storage nodes.
-const systemEncoder = createEncoder({ systemTopic })
+const systemEncoder = createEncoder({ contentTopic: systemTopic })
 const systemDecoder = createDecoder(systemTopic)
-
-// Create a message structure using Protobuf
-const ChatMessage = new protobuf.Type("ChatMessage")
-  .add(new protobuf.Field("timestamp", 1, "uint64"))
-  .add(new protobuf.Field("sender", 2, "string"))
-  .add(new protobuf.Field("message", 3, "string"))
 
 const SystemMessage = new protobuf.Type("SystemMessage")
   .add(new protobuf.Field("timestamp", 1, "uint64"))
@@ -39,54 +45,70 @@ const SystemMessage = new protobuf.Type("SystemMessage")
   .add(new protobuf.Field("args", 4, "bytes"))
   .add(new protobuf.Field("signature", 5, "bytes"))
 
+export async function subscribeToSystem(node, msgCallback) {
+  return subscribeToTopic(node, systemDecoder, (payload) => {
+    msgCallback(SystemMessage.decode(payload))
+  })
+}
+
+export async function sendSystemMessage(node, payload) {
+  await sendMessage(node, systemEncoder, SystemMessage, payload)
+}
+
+// =================================================================================================
+
 /**
- * TODO
+ * Starts and return a waku node. Relay node for now.
  */
-export async function startWaku(signalStatus, msgCallback) {
+export async function startWakuNode() {
   const node = await createRelayNode({
     // Emits messages to self too.
     emitSelf: true,
-    // TODO: do we need this?
-    // defaultBootstrap: true,
+    // Plot twist: default bootstrap is not actually the default...
+    defaultBootstrap: true,
   })
 
-  // In case we want to run a light node instead.
-  const node = await createLightNode()
-  try {
-    // TODO lookup docs of this
-    await getPeers()
-  } catch (e) {
-    signalStatus("Failed to find a peer")
-  }
+  // // In case we want to run a light node instead.
+  // const node = await createLightNode()
+  // try {
+  //   // TODO lookup docs of this
+  //   await getPeers()
+  // } catch (e) {
+  //   signalStatus("Failed to find a peer")
+  // }
 
   await node.start()
 
   // For debugging
   window.waku = node
 
-  // Add a hook to process all incoming messages on a specified content topic.
+  return node
+}
 
-  const subscriptionCallback = (message) => {
+// =================================================================================================
+
+/**
+ * Subscribe the node to the given topic, calling `msgCallback` with the message payload if it
+ * exists. Returns an unsubscribe function.
+ */
+export async function subscribeToTopic(node, decoder, payloadCallback) {
+  return node.relay.subscribe(decoder, (message) => {
     // Checks there is a payload on the message.
     // Waku Message is encoded in protobuf, in proto v3 fields are always optional.
     if (!message.payload) return
+    payloadCallback(message.payload)
+  })
+}
 
-    const messageObj = SystemMessage.decode(message.payload)
+// =================================================================================================
 
-    // For debugging
-    console.dir(messageObj)
-    msgCallback(decodedMessage)
-  }
+/**
+ * Example function for the waku setup that subscribes to the system topic.
+ */
+export async function setupWaku(signalStatus, msgCallback) {
 
-  node.relay.subscribe(
-    systemDecoder,
-    subscriptionCallback,
-    [systemTopic])
-
-  // // For light node
-  // const subscription = await node.filter.createSubscription()
-  // await subscription.subscribe([systemDecoder], subscriptionCallback)
-
+  const node = await startWakuNode()
+  await subscribeToSystem(node, msgCallback)
   signalStatus("Connecting to a peer")
 
   // Best effort method that waits for the Waku node to be connected to remote
@@ -102,6 +124,22 @@ export async function startWaku(signalStatus, msgCallback) {
   return node
 }
 
+// =================================================================================================
+
+export async function shutdownWaku(node) {
+  // // For light node
+  // await subscription.unsubscribe([systemTopic])
+  // node.relay.unsubscribe([systemTopic])
+
+  // Should work for both relay and light nodes (but verify for light node)
+  for (const subscription of node.relay.toSubscriptionIterator()) {
+    await subscription.unsubscribe([systemTopic])
+  }
+  await node.stop()
+}
+
+// =================================================================================================
+
 // // Example
 // const msgType = ChatMessage
 // const msgPayload = {
@@ -110,31 +148,10 @@ export async function startWaku(signalStatus, msgCallback) {
 //   message: "Hello, World!",
 // }
 
-export async function sendMessage(node, msgType, msgPayload) {
+export async function sendMessage(node, encoder, msgType, msgPayload) {
   const protoMessage = msgType.create(msgPayload)
   const serialisedMessage = msgType.encode(protoMessage).finish()
-  await waku.relay.send(systemEncoder, { payload: serialisedMessage })
+  await node.relay.send(encoder, { payload: serialisedMessage })
 }
 
-export async shutdownWaku(node) {
-  // // For light node
-  // await subscription.unsubscribe([systemTopic])
-  // node.relay.unsubscribe([systemTopic])
-
-  // Should work for both relay and light nodes (but verify for light node)
-  for (const subscription of node.relay.toSubscriptionIterable()) {
-    await subscription.unsubscribe([systemTopic])
-  }
-  await node.stop()
-}
-
-// === REFERENCE ===
-
-// // Get list of peers (for use with light node)
-// await node.libp2p.peerStore.all()
-
-// // Callback on new peer connection (for use with light node)
-// node.store.addLibp2pEventListener("peer:connect", async (event) => {
-//   const peerId = event.detail
-//   console.log(`Peer connected with peer id: ${peerId}`)
-// })
+// =================================================================================================
