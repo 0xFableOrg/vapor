d2014274ae2bae6f974583772aa8db6329daa882
ultraviolet10 <aritrac1998@hotmail.com>
2023-11-18 19:08:51 +0300

    fix: chat section style

--- packages/ui/components/Lobby/LobbyChat.tsx
+++ packages/ui/components/Lobby/LobbyChat.tsx
@@ -4,26 +4,21 @@ interface LobbyChatProps {}
 
 const LobbyChat: React.FC<LobbyChatProps> = () => {
   return (
-    <div className="flex flex-col items-start justify-start p-4 w-[50%] h-full border-[1px] border-white rounded-xl space-y-3">
-      <span className="text-white font-capian text-[20px]">chat</span>
-      <div className="flex flex-col rounded-xl space-y-2 bg-gray-700 h-[65%] w-full relative">
-        <div
-          className="absolute inset-0 bg-cover bg-center z-0"
-          style={{
-            backgroundImage: "url(/img/chatBg.jpeg)",
-            opacity: 0.4,
-          }}
-        ></div>
+    <div
+      className="flex flex-col items-start justify-start p-4 w-[50%] h-full border-[1px] border-white rounded-xl space-y-3 relative"
+      style={{
+        backgroundImage: "url(/img/chatBg.jpeg)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <span className="text-white font-capian text-[20px] z-10">chat</span>
+      <div className="flex flex-col rounded-xl space-y-2 bg-gray-700 bg-opacity-50 h-[65%] w-full z-10">
+        {/* Content goes here */}
       </div>
-      <div className="flex w-full h-[15%] rounded-xl">
+      <div className="flex w-full h-[15%] rounded-xl z-10">
         <textarea
-          //   value={message}
-          //   onChange={handleMessageChange}
           placeholder={`Type your message here...`}
-          //   onKeyDown={keyPressed}
-          //   rows={1}
-          //   cols={1}
-          //   ref={textAreaRef}
           className="rounded-xl w-full bg-gray-300"
         />
       </div>
