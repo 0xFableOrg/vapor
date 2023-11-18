72c53acff23283237989b242b9106c9f3b87f8d5
cheechyuanang <angchee@live.com.my>
2023-11-18 15:11:08 +0300

    add sdk list games and lobbies

--- packages/sdk/index.ts
+++ packages/sdk/index.ts
@@ -1,14 +1,1 @@
-interface IVapor {
-  startGame(): void;
-  endGame(): void;
-}
-
-export class Vapor implements IVapor {
-  constructor(private readonly vaporContract: string) {}
-  startGame(): void {
-    throw new Error("Method not implemented.");
-  }
-  endGame(): void {
-    throw new Error("Method not implemented.");
-  }
-}
+export * from "./vapor";
