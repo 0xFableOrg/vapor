809b6434fcc627ab7520a7799300be80388c4031
cheechyuanang <angchee@live.com.my>
2023-11-19 06:27:40 +0300

    get proof

--- packages/sdk/helper/api.ts
+++ packages/sdk/helper/api.ts
@@ -4,3 +4,8 @@ export async function post<T>(url: string, body?: any) {
   const res = await axios.post<T>(url, body);
   return res;
 }
+
+export async function get<T>(url: string) {
+  const res = await axios.get<T>(url);
+  return res;
+}
