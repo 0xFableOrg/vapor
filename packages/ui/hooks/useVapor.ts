d12b016f6c8ed96f89c34da3bca56474cd0e2676
chen <23054115+cwkang1998@users.noreply.github.com>
2023-11-19 12:15:04 +0800

    fix: incorrect signer usage

--- packages/ui/hooks/useVapor.ts
+++ packages/ui/hooks/useVapor.ts
@@ -1,28 +1,35 @@
-import { useEffect, useState } from "react"
-import { useStore } from "@store/store"
-import { StoreActionTypes } from "@type/store"
-import { Vapor } from "@vapor/sdk"
-import { VoidSigner, providers } from "ethers"
+import { useEffect, useState } from "react";
+import { useStore } from "@store/store";
+import { StoreActionTypes } from "@type/store";
+import { Vapor } from "@vapor/sdk";
+import { VoidSigner, providers } from "ethers";
 
 export const useVapor = (vaporContractAddr: string) => {
-  const [isReady, setIsReady] = useState<boolean>(false)
-  const { store, dispatch } = useStore()
-  const { provider, account } = store
+  const [isReady, setIsReady] = useState<boolean>(false);
+  const { store, dispatch } = useStore();
+  const { provider, account } = store;
 
   useEffect(() => {
-    const asyncFn = async (provider: providers.Provider, account: string) => {
-      const signer = new VoidSigner(account, provider)
+    const asyncFn = async (
+      provider: providers.AlchemyProvider | providers.Web3Provider,
+      account: string
+    ) => {
       dispatch({
-        payload: { vaporInstance: new Vapor(vaporContractAddr, signer) },
+        payload: {
+          vaporInstance: new Vapor(
+            vaporContractAddr,
+            provider.getSigner(account)
+          ),
+        },
         type: StoreActionTypes.SET_VAPOR_INSTANCE,
-      })
-      setIsReady(true)
-    }
+      });
+      setIsReady(true);
+    };
 
     if (provider && account) {
-      asyncFn(provider, account)
+      asyncFn(provider, account);
     }
-  }, [provider, account, vaporContractAddr, dispatch])
+  }, [provider, account, vaporContractAddr, dispatch]);
 
-  return { vapor: store.vaporInstance, isReady: isReady }
-}
+  return { vapor: store.vaporInstance, isReady: isReady };
+};
